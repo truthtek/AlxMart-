e2059f7441d2a703b4ac995a38f8dbd746c96983
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-09-30 06:42:18 +0100

    Create Footer.js

---
+++ mart/src/components/Footer.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white p-4 text-center">
+      <p>&copy; 2024 AlxMart - All Rights Reserved</p>
+      <p>This is my final project for Alx cohort 19 Alxswe Program</p>
+    </footer>
+  );
+}
+
+export default Footer;
